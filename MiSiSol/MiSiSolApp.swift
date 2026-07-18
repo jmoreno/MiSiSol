252eb7a5df20f6e984c21b43ce4907c419a87ad0
Javi Moreno <jmoreno.zn@gmail.com>
2026-07-18 09:33:48 +0200

    Initial Commit

---
+++ MiSiSol/MiSiSolApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiSiSolApp.swift
+//  MiSiSol
+//
+//  Created by Javi Moreno on 18/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct MiSiSolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MiSiSolTests/MiSiSolTests.swift
@@ -0,0 +1,19 @@
+//
+//  MiSiSolTests.swift
+//  MiSiSolTests
+//
+//  Created by Javi Moreno on 18/07/2026.
+//
+
+import Testing
+@testable import MiSiSol
+
+struct MiSiSolTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
